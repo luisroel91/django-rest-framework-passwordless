8e3621145b2b3a2f3d6a0d5a32ad1517add0aa8f
luisroel91 <44761184+luisroel91@users.noreply.github.com>
2019-07-20 12:51:14 -0400

    Update urls.py

--- drfpasswordless/urls.py
+++ drfpasswordless/urls.py
@@ -9,9 +9,9 @@ from drfpasswordless.views import (
 )
 
 urlpatterns = [
-     path('callback/auth/', ObtainAuthTokenFromCallbackToken.as_view(), name='auth_callback'),
-     path('auth/email/', ObtainEmailCallbackToken.as_view(), name='auth_email'),
-     path('auth/mobile/', ObtainMobileCallbackToken.as_view(), name='auth_mobile'),
+     path('callback/', ObtainAuthTokenFromCallbackToken.as_view(), name='auth_callback'),
+     path('email/', ObtainEmailCallbackToken.as_view(), name='auth_email'),
+     path('mobile/', ObtainMobileCallbackToken.as_view(), name='auth_mobile'),
      path('callback/verify/', VerifyAliasFromCallbackToken.as_view(), name='verify_callback'),
      path('verify/email/', ObtainEmailVerificationCallbackToken.as_view(), name='verify_email'),
      path('verify/mobile/', ObtainMobileVerificationCallbackToken.as_view(), name='verify_mobile'),
